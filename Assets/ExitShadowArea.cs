56373d3f7ee19d047cf6e96a116b47cf2180fc1b
Rodolfo <rodolfomsouza@gmail.com>
2019-12-31 00:29:34 -0300

    Shadow Area

---
+++ Assets/ExitShadowArea.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitShadowArea : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Player Death");
+        collision.gameObject.GetComponent<CollisionPlayer>().KillPlayer();
+    }
+}
