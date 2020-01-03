8ffd2df818a3a82d670ce2bc3b22063388f68474
Rodolfo <rodolfomsouza@gmail.com>
2020-01-03 12:47:02 -0300

    Scene Menu and Scene History

---
+++ Assets/CanvasHistory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasHistory : MonoBehaviour
+{
+    public void StartSceneGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject canvasMenu;
+    [SerializeField] GameObject canvasCredits;
+    [SerializeField] GameObject canvasTransition;
+
+    bool isPlaying = false;
+
+    public void ClickButtonCredits()
+    {
+        if (!isPlaying){ canvasCredits.SetActive(true); }
+    }
+
+    public void ClickButtonPlay()
+    {
+        isPlaying = true;
+        canvasTransition.SetActive(true);
+        Invoke("StartGame", 1.4f);
+    }
+
+    public void ClickButtonExitGame()
+    {
+        if (!isPlaying){ Application.Quit(); }
+    }
+
+    public void CloseCanvasCredits()
+    {
+        canvasCredits.SetActive(false);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
