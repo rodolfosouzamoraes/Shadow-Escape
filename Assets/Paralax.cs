b07809e7c38a87353adf771814e1dfaa12dd966d
Rodolfo <rodolfomsouza@gmail.com>
2020-01-09 13:58:19 -0300

    World Scene

---
+++ Assets/Paralax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public Material material;
+    public float speed = 0.05f;
+    private float _offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _offset += speed * Time.deltaTime;
+        material.SetTextureOffset("_MainTex", new Vector2(_offset, 0));
+    }
+
+    public void ParaParalax()
+    {
+        speed = 0;
+    }
+
+    public void ContinuaParalax()
+    {
+        speed = 0.05f;
+    }
+}
