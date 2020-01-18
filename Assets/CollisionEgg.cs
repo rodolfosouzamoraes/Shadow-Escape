2556338190d708eeac8c061b00ce5aece7b24bd8
Rodolfo <rodolfomsouza@gmail.com>
2020-01-18 18:30:34 -0300

    Easter Egg Boss

---
+++ Assets/CollisionEgg.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionEgg : MonoBehaviour
+{
+    int totalBatteryInGame;
+    int totalCaverudo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalBatteryInGame = GameObject.FindGameObjectsWithTag("Battery").Length;
+        totalCaverudo = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<MovimentPlayer>().isAlive)
+        {
+            PlayerPrefs.SetInt("TotalBatteryCollected", totalBatteryInGame);
+            PlayerPrefs.SetInt("CaverudosDeath", totalCaverudo);
+            SceneManager.LoadScene(3);
+        }
+        
+    }
+}
